56d71a6c147450f8e14878b929e77399b92cb42e
TGExplore <52099763+TGExplore@users.noreply.github.com>
2019-09-09 23:32:24 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -227,7 +227,7 @@ __help__ = """
  - /demote: demotes the user replied to
 """
 
-__mod_name__ = "🧡Admin🧡"
+__mod_name__ = "Admin"
 
 PIN_HANDLER = CommandHandler("pin", pin, pass_args=True, filters=Filters.group)
 UNPIN_HANDLER = CommandHandler("unpin", unpin, filters=Filters.group)
