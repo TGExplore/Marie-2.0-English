7434c3dcf4e30209a62ffb28bd25065ba1f99388
TGExplore <52099763+TGExplore@users.noreply.github.com>
2019-09-10 16:50:00 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -91,7 +91,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
         chat.kick_member(user_id)
         bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
         keyboard = []
-        reply = "{} ന് ബണ്ണ് കൊടുത്തു വിട്ടിട്ടുണ്ട് !".format(mention_html(member.user.id, member.user.first_name))
+        reply = "{} Banned!".format(mention_html(member.user.id, member.user.first_name))
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
