506a821417200b0c31cd70501328c8a9cf4317dc
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-03 01:05:46 +0530

    Update feds_sql.py

--- tg_bot/modules/sql/feds_sql.py
+++ tg_bot/modules/sql/feds_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from haruka.modules.sql import SESSION, BASE
+from tg_bot.modules.sql import SESSION, BASE
 
 
 class Federations(BASE):
