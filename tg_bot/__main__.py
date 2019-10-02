d474820227b647d465c3a654e4d602bdef9f7174
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-03 01:25:57 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -140,7 +140,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN)
     else:
-        update.effective_message.reply_text("waked up😏😏😏")
+        update.effective_message.reply_text("Yo,I'm alive👩")
 
 
 # for test purposes
