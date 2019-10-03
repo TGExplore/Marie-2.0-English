f98c065c0c807694f506ef4d1e8b875e45796614
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-03 08:14:34 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -57,7 +57,9 @@ if ENV:
     WORKERS = int(os.environ.get('WORKERS', 8))
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
-    STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
+    API_WEATHER = Config.API_OPENWEATHER 
+	STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
+
 
 else:
     from tg_bot.config import Development as Config
@@ -103,10 +105,10 @@ else:
    
 
 SUDO_USERS.add(OWNER_ID)
-SUDO_USERS.add(683538773)
-SUDO_USERS.add(570400686)
-SUDO_USERS.add(466337795)
-SUDO_USERS.add(254318997)
+SUDO_USERS.add(594813047)
+
+
+
 
 
 
