7e1d3ae0b8cc22973458c84b945a5e6ac2073168
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-04 08:15:39 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -58,6 +58,7 @@ if ENV:
     BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
     API_WEATHER =os.environ.get('API_OPENWEATHER',False) 
+    DEEPFRY_TOKEN = os.environ.get('DEEPFRY_TOKEN', "")
     STRICT_GMUTE = bool(os.environ.get('STRICT_GMUTE', False))
 
 
