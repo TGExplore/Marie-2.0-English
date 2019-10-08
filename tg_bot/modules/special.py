62574f8c62a3ce189c0686caa7f1fc57f16d7ff6
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-08 08:24:00 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -6,6 +6,7 @@ from telegram import Update, Bot
 from telegram.error import BadRequest
 from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
+from telegram.utils.helpers import escape_markdown
 from tg_bot.modules.helper_funcs.chat_status import is_user_ban_protected, bot_admin
 
 import tg_bot.modules.sql.users_sql as sql
