440d1ce32d990500208498656cb23ddc039c2eab
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-08 08:31:24 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -4,6 +4,7 @@ from typing import Optional, List
 from telegram import TelegramError, Chat, Message
 from telegram import Update, Bot
 from telegram.error import BadRequest
+from telegram import ParseMode
 from telegram.ext import MessageHandler, Filters, CommandHandler
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown
