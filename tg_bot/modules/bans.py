5433c93f74345a535a346dc8446d8a874faa354b
Muttahir <38470944+Muttahir786@users.noreply.github.com>
2019-10-09 08:51:33 -0500

    ok fixed derp

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -92,7 +92,6 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
     try:
         chat.kick_member(user_id)
         keyboard = []
-        bot.send_sticker(update.effective_chat.id, BAN_STICKER)
         message.reply_text(reply, reply_markup=keyboard, parse_mode=ParseMode.HTML)
         return log
 
