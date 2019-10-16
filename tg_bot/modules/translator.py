f49fc03ed501cf1f20722692b0681071628fc924
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-16 23:29:16 +0530

    Create translator.py

---
+++ tg_bot/modules/translator.py
@@ -0,0 +1,34 @@
+from typing import Optional, List
+
+from telegram import Message, Update, Bot, User
+from telegram import MessageEntity
+from telegram.ext import Filters, MessageHandler, run_async
+
+from tg_bot import dispatcher, LOGGER
+from tg_bot.modules.disable import DisableAbleCommandHandler
+
+from googletrans import Translator
+
+
+@run_async
+def do_translate(bot: Bot, update: Update, args: List[str]):
+    short_name = "Created By @MidukkiBot 😬"
+    msg = update.effective_message # type: Optional[Message]
+    lan = " ".join(args)
+    to_translate_text = msg.reply_to_message.text
+    translator = Translator()
+    try:
+        translated = translator.translate(to_translate_text, dest=lan)
+        src_lang = translated.src
+        translated_text = translated.text
+        msg.reply_text("Translated from {} to {}.\n {}".format(src_lang, lan, translated_text))
+    except:
+        msg.reply_text("Error")
+
+
+__help__ = """- /tr (language code) as reply to a long message.
+"""
+__mod_name__ = "Translator"
+
+dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
+© 2019 GitHub, Inc.
