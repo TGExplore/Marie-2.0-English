849ff1d2d201bc47a9cc864c86874751d86e8efc
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-16 23:32:22 +0530

    Update translator.py

--- tg_bot/modules/translator.py
+++ tg_bot/modules/translator.py
@@ -31,4 +31,4 @@ __help__ = """- /tr (language code) as reply to a long message.
 __mod_name__ = "Translator"
 
 dispatcher.add_handler(DisableAbleCommandHandler("tr", do_translate, pass_args=True))
-© 2019 GitHub, Inc.
+
