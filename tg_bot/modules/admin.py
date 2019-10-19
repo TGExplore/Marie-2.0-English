3499442a173abb1e93bffa9c753ef03272006899
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-19 22:15:49 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -224,7 +224,7 @@ def adminlist(bot: Bot, update: Update):
     for admin in administrators:
         user = admin.user
         status = admin.status
-        name = escape_markdown("@" + user.username)
+        name = "[{}](tg://user?id={})".format(user.first_name + " " + (user.last_name or ""), user.id)
         if user.username:
             name = escape_markdown("@" + user.username)
         if status == "administrator":
