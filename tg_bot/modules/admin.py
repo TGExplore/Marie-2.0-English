ebb77da1c582fafa7aa9ac4a372eecdb5f6f9d6f
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-19 21:46:46 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -14,6 +14,7 @@ from tg_bot.modules.helper_funcs.chat_status import bot_admin, can_promote, user
 from tg_bot.modules.helper_funcs.extraction import extract_user
 from tg_bot.modules.log_channel import loggable
 from tg_bot.modules.connection import connected
+from tg_bot.modules.translations.strings import tld
 
 @run_async
 @bot_admin
