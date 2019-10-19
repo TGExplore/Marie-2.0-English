f53e43ecabcc5621e003b335b733125e30ad2655
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-19 20:45:38 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -13,6 +13,7 @@ from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin
 from tg_bot.modules.helper_funcs.extraction import extract_user
 from tg_bot.modules.log_channel import loggable
+from tg_bot.modules.connection import connected
 
 @run_async
 @bot_admin
