4044544d7ef07d6c87bfc02cd3cb943e533f1673
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-19 19:59:59 +0530

    Lol let see

--- tg_bot/modules/sed.py
+++ tg_bot/modules/sed.py
@@ -111,7 +111,7 @@ eg: \\?.
 
 __mod_name__ = "Sed/Regex"
 
+SED_HANDLER = CommandHandler("sed", sed, pass_args=True, filters=Filters.user(OWNER_ID))
 
-SED_HANDLER = DisableAbleRegexHandler(r's([{}]).*?\1.*'.format("".join(DELIMITERS)), sed, friendly="sed")
 
 dispatcher.add_handler(SED_HANDLER)
