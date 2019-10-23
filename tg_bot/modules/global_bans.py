aa7a0002d9de54ef34385c1b8831e31db4074c5f
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-10-23 19:40:44 +0530

    Lol ok

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -164,6 +164,9 @@ def gban(bot: Bot, update: Update, args: List[str]):
     send_to_list(bot, SUDO_USERS + SUPPORT_USERS,
                    "{} has been successfully gbanned :)".format(mention_html(user_chat.id, user_chat.first_name or "Deleted Account")),
                    html=True)  
+    text13 = f" User {} Gbanned :)".format(mention_html(user_chat.id, user_chat.first_name or "Deleted Account")),
+                   html=True)  
+    update.effective_message.reply_text(text13, parse_mode=ParseMode.HTML)
 
 @run_async
 def ungban(bot: Bot, update: Update, args: List[str]):
