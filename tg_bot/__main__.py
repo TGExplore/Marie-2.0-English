ca0826554bd3f84d9d8e0387b99c47b7f61acffa
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-09 11:52:03 +0530

    Maybe work

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,8 +142,11 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN)
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Support Group",
+                                                                       url="t.me/ctrlsupport".format(bot.username))]]))
     else:
+                
+    
         update.effective_message.reply_text("Yo,I'm alive👩")
 
 
