8c93f9cb5e80a4ed5058cc73ae99dd2c9170da71
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-16 08:07:26 +0530

    Updated strings

--- tg_bot/modules/userinfo.py
+++ tg_bot/modules/userinfo.py
@@ -68,7 +68,7 @@ def about_bio(bot: Bot, update: Update, args: List[str]):
         username = user.first_name
         update.effective_message.reply_text("{} No details about him have been added yet !".format(username))
     else:
-        update.effective_message.reply_text(" Your information about you has been added !")
+        update.effective_message.reply_text("You haven't had a bio set about yourself yet!")
 
 
 @run_async
@@ -93,13 +93,13 @@ def set_about_bio(bot: Bot, update: Update):
         if len(bio) == 2:
             if len(bio[1]) < MAX_MESSAGE_LENGTH // 4:
                 sql.set_user_bio(user_id, bio[1])
-                message.reply_text("{} Information about the him has been successfully collected !".format(repl_message.from_user.first_name))
+                message.reply_text("Updated's {} Bio !".format(repl_message.from_user.first_name))
             else:
                 message.reply_text(
                     "About you {} Must stick to the letter! The number of characters you have just tried {} hm .".format(
                         MAX_MESSAGE_LENGTH // 4, len(bio[1])))
     else:
-        message.reply_text(" His information can only be added if someone's MESSAGE as a REPLY")
+        message.reply_text(" Reply to someone's message to set their bio! ")
 
 
 def __user_info__(user_id):
