5735fc19b70821bf096692034342536be06d93c4
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-16 21:12:52 +0530

    Update antiflood_sql.py

--- tg_bot/modules/sql/antiflood_sql.py
+++ tg_bot/modules/sql/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from emilia.modules.sql import SESSION, BASE
+from tg_bot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
