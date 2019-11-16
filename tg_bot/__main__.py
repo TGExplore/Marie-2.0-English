033c41aa5333b353fcb008afcf42e627715e479a
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-11-16 09:09:59 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -144,7 +144,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Support Group",
                                                                        url="t.me/ctrlsupport".format(bot.username))]]))
-        else:
+    else:
             send_start(bot, update)
                 
         else:
