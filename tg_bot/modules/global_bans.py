aa2ed26140e4959efad8fe47f0fb0b936dff71ce
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2019-12-20 20:10:02 +0530

    Syntax error

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -294,7 +294,7 @@ def gbanstat(bot: Bot, update: Update, args: List[str]):
             sql.disable_gbans(update.effective_chat.id)
             update.effective_message.reply_text("I've disabled gbans in this Group. GBans wont affect your users "
                                                 "anymore. You'll be less protected from any trolls and spammers "
-                                                "though".
+                                                "though!")
     else:
         update.effective_message.reply_text("Give me some arguments to choose a setting! on/off, yes/no!\n\n"
                                             "Your current setting is: {}\n"
