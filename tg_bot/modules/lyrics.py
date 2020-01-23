0c8d31121623f373d408cb2f9661117756227ba3
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2020-01-23 12:44:02 +0530

    Thanks Phoenix

--- tg_bot/modules/lyrics.py
+++ tg_bot/modules/lyrics.py
@@ -1,49 +1,52 @@
-from PyLyrics import *
-from telegram import Update, Bot
+# Simple lyrics module using tswift by @TheRealPhoenix
+
+from tswift import Song
+
+from telegram import Bot, Update, Message, Chat
 from telegram.ext import run_async
-from typing import Optional, List
 
-from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
 
-from requests import get
-
-LYRICSINFO = "\n[Full Lyrics](http://lyrics.wikia.com/wiki/%s:%s)"
 
 @run_async
-def lyrics(bot: Bot, update: Update, args: List[str]):
-    message = update.effective_message
-    text = message.text[len('/lyrics '):]
-    song = " ".join(args).split("- ")
-    reply_text = f'Looks up for lyrics'
-    
-    if len(song) == 2:
-        while song[1].startswith(" "):
-            song[1] = song[1][1:]
-        while song[0].startswith(" "):
-            song[0] = song[0][1:]
-        while song[1].endswith(" "):
-            song[1] = song[1][:-1]
-        while song[0].endswith(" "):
-            song[0] = song[0][:-1]
-        try:
-            lyrics = "\n".join(PyLyrics.getLyrics(
-                song[0], song[1]).split("\n")[:20])
-        except ValueError as e:
-            return update.effective_message.reply_text("Song %s not found :(" % song[1], failed=True)
-        else:
-            lyricstext = LYRICSINFO % (song[0].replace(
-                " ", "_"), song[1].replace(" ", "_"))
-            return update.effective_message.reply_text(lyrics + lyricstext, parse_mode="MARKDOWN")
+def lyrics(bot: Bot, update: Update, args):
+    msg = update.effective_message
+    query = " ".join(args)
+    song = ""
+    if not query:
+        msg.reply_text("You haven't specified which song to look for!")
+        return
     else:
-        return update.effective_message.reply_text("Invalid syntax- try Artist - Title!", failed=True)
-
-
+        song = Song.find_song(query)
+        if song:
+            if song.lyrics:
+                reply = song.format()
+            else:
+                reply = "Couldn't find any lyrics for that song!"
+        else:
+            reply = "Song not found!"
+        if len(reply) > 4090:
+            with open("lyrics.txt", 'w') as f:
+                f.write(f"{reply}\n\n\nOwO UwU OmO")
+            with open("lyrics.txt", 'rb') as f:
+                msg.reply_document(document=f,
+                caption="Message length exceeded max limit! Sending as a text file.")
+        else:
+            msg.reply_text(reply)
+                
+        
+                
 __help__ = """
- - /lyrics <keyword> Find your favourite songs' lyrics
+Want to get the lyrics of your favorite songs straight from the app? This module is perfect for that!
+*Available commands:*
+ - /lyrics <song>: returns the lyrics of that song.
+ You can either enter just the song name or both the artist and song name.
 """
 
 __mod_name__ = "Lyrics"
+
+
 
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
 
