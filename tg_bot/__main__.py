af07fa807fa5b6068d0593522cfcb5202a802ce0
Whit3diamond <52831032+Whit3diamond@users.noreply.github.com>
2020-04-08 15:19:57 +0300

    a

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -40,7 +40,7 @@ Hello! my name *{}*.
 *Main* available commands:
  - /start: Start the bot...
  - /help: help....
- - /donate: To find out more about donating!
+ 
  - /settings:
    - in PM: To find out what SETTINGS you have set....
    - in a group:
@@ -49,11 +49,7 @@ Hello! my name *{}*.
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
 
-DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+DONATE_STRING = """Don't use haruka."""
 
 IMPORTED = {}
 MIGRATEABLE = []
