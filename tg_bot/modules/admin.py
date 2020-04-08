0a9bdcc8ea4602b0ed785b05720d09fc17e13da7
Whit3diamond <52831032+Whit3diamond@users.noreply.github.com>
2020-04-08 17:23:49 +0300

    rights

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -48,7 +48,7 @@ def promote(bot: Bot, update: Update, args: List[str]) -> str:
                           can_post_messages=bot_member.can_post_messages,
                           can_edit_messages=bot_member.can_edit_messages,
                           can_delete_messages=bot_member.can_delete_messages,
-                          # can_invite_users=bot_member.can_invite_users,
+                          can_invite_users=bot_member.can_invite_users,
                           can_restrict_members=bot_member.can_restrict_members,
                           can_pin_messages=bot_member.can_pin_messages,
                           can_promote_members=bot_member.can_promote_members)
