7ec5612d9bd3f2623df73b884da2da8990c6266f
Whit3diamond <52831032+Whit3diamond@users.noreply.github.com>
2020-04-10 22:21:24 +0300

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -3,9 +3,9 @@ class Config(object):
     LOGGER = "True"
 
     # REQUIRED
-    API_KEY = "1203217513:AAFbw0a6gCekhTGVWmPE0l8SsAMWF1Tc5cI"
-    OWNER_ID = "1142196004" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "loopsword"
+    API_KEY = ""
+    OWNER_ID = "" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = ""
 
     # RECOMMENDED
     MESSAGE_DUMP = "None"  # needed to make sure 'save from' messages persist
@@ -13,9 +13,9 @@ class Config(object):
     NO_LOAD = ['translation', 'rss']
 
     # OPTIONAL
-    SUDO_USERS = [1142196004 722278787 805491896 666670963]  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = [1142196004 722278787 805491896 758233009 666670963 955227320]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = [1142196004 722278787 805491896 758233009 666670963]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = "None"  # EG, paypal
     CERT_PATH = "None"
     DEL_CMDS = "False"  # Whether or not you should delete "blue text must click" commands
