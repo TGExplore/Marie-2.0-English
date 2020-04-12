34de4b6b47c5076f618aeccffdb7f11d73b05b2a
Whit3diamond <52831032+Whit3diamond@users.noreply.github.com>
2020-04-12 11:57:26 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,7 +20,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 Hi {}, my name is Melody! if you have any questions about how to use me please give me /help... 
 
-im a group manager bot maintained by  [this person](t.me/Blanccy).
+im a group manager bot maintained by  [this person](t.me/gleamglow).
 
 My future updates will be in  [this group](t.me/melodytgsupport)
 
