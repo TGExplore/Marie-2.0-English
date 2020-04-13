41b6885fc932eee55f21e3f2c7ab99d52359e3ee
Whit3diamond <52831032+Whit3diamond@users.noreply.github.com>
2020-04-13 16:26:46 +0300

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -1,16 +1,26 @@
+if not __name__.endswith("sample_config"):
+    import sys
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+          "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
+    quit(1)
+
+
 # Create a new config.py file in same dir and import, then extend this class.
 class Config(object):
-    LOGGER = "True"
+    LOGGER = True
 
     # REQUIRED
-    API_KEY = ""
+    API_KEY = "" #You will get this from botfather named bot token
     OWNER_ID = "" # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = ""
 
     # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = "None"  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
+    WEBHOOK = "False"
+    URL = "None"
 
     # OPTIONAL
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
@@ -18,15 +28,17 @@ class Config(object):
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = "None"  # EG, paypal
     CERT_PATH = "None"
+    PORT = 5000
     DEL_CMDS = "False"  # Whether or not you should delete "blue text must click" commands
-    STRICT_GBAN = "True"
+    STRICT_GBAN = "False"
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
     ALLOW_EXCL = "False"  # Allow ! commands as well as /
 
 
 class Production(Config):
-    LOGGER = "False"
+    LOGGER = False
 
 
 class Development(Config):
-    LOGGER = "True"
+    LOGGER = True
