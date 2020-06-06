0c1aac0a80010acba4e40875a89953dd49313e39
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2020-06-06 19:50:22 +0530

    Update alternate.py

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -1,40 +1,29 @@
-import sys
-import traceback
-
 from functools import wraps
-from typing import Optional
 
-from telegram import User, Chat, ChatMember, Update, Bot
-from telegram import error
+from telegram import User, Chat, ChatMember, Update
+from telegram import error, ChatAction
 
-from tg_bot import dispatcher, DEL_CMDS, SUDO_USERS, WHITELIST_USERS, LOGGER, MESSAGE_DUMP
+from tg_bot import DEL_CMDS, SUDO_USERS
 
-from tg_bot.modules import translations
 
-def send_message(message, text, target_id=None, *args,**kwargs):
-	if not target_id:
-		try:
-			return message.reply_text(text, *args,**kwargs)
-		except error.BadRequest as err:
-			if str(err) == "Reply message not found":
-				try:
-					return message.reply_text(text, quote=False, *args, **kwargs)
-				except error.BadRequest as err:
-					LOGGER.exception("ERROR: {}".format(err))
-			elif str(err) == "Have no rights to send a message":
-				try:
-					dispatcher.bot.leaveChat(message.chat.id)
-					dispatcher.bot.sendMessage(MESSAGE_DUMP, "I left `{}`\nBecause of: `Muted`".format(message.chat.title))
-				except error.BadRequest as err:
-					if str(err) == "Chat not found":
-						pass
-			else:
-				LOGGER.exception("ERROR: {}".format(err))
-	else:
-		try:
-			dispatcher.bot.send_message(target_id, text, *args, **kwarg)
-		except error.BadRequest as err:
-			LOGGER.exception("ERROR: {}".format(err))
+def send_message(message, text,  *args,**kwargs):
+	try:
+		return message.reply_text(text, *args,**kwargs)
+	except error.BadRequest as err:
+		if str(err) == "Reply message not found":
+			return message.reply_text(text, quote=False, *args,**kwargs)
+
+
+def typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+        return func(update, context,  *args, **kwargs)
+
+    return command_func
+
 
 def send_action(action):
     """Sends `action` while processing func command."""
