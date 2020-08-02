d28b96ef28b83920214d2ae854467afc9fc23adf
RysepProject <62201395+RysepProject@users.noreply.github.com>
2020-08-02 20:22:39 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,9 +22,9 @@ hoi {}, my name is {}! if you have any questions about how to use me please give
 
 im a group manager bot maintained by  [this person](tg://user?id={}).
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+My future updates will be put into This Channel - @ZyeeProjexts
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
+This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/RysepProject/AnimoHikaru),
 you can create clone same like me..
 
 For more commands click /help...
