5fa400e63b9547fd1ea1f1f53d2dd3cedfec6fe0
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-11 19:55:22 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+hoi {}, my name is {}! if you have any naveen about how to use me please give me /help... 
 
 im a group manager bot maintained by  [this person](tg://user?id={}).
 
