79ff25c0a397216b655990650aabee1569eaabf5
Avatar <50352435+AVATARX0TIC@users.noreply.github.com>
2020-09-06 10:53:25 +0530

    Update antiflood_sql.py

--- tg_bot/modules/sql/antiflood_sql.py
+++ tg_bot/modules/sql/antiflood_sql.py
@@ -1,8 +1,8 @@
 import threading
 
-from sqlalchemy import Column, Integer, String, UnicodeText
+from sqlalchemy import String, Column, Integer, UnicodeText
 
-from tg_bot.modules.sql import BASE, SESSION
+from tg_bot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
@@ -22,6 +22,7 @@ class FloodControl(BASE):
     def __repr__(self):
         return "<flood control for %s>" % self.chat_id
 
+
 class FloodSettings(BASE):
     __tablename__ = "antiflood_settings"
     chat_id = Column(String(14), primary_key=True)
@@ -40,14 +41,14 @@ class FloodSettings(BASE):
 FloodControl.__table__.create(checkfirst=True)
 FloodSettings.__table__.create(checkfirst=True)
 
-INSERTION_LOCK = threading.RLock()
+INSERTION_FLOOD_LOCK = threading.RLock()
 INSERTION_FLOOD_SETTINGS_LOCK = threading.RLock()
 
 CHAT_FLOOD = {}
 
 
 def set_flood(chat_id, amount):
-    with INSERTION_LOCK:
+    with INSERTION_FLOOD_LOCK:
         flood = SESSION.query(FloodControl).get(str(chat_id))
         if not flood:
             flood = FloodControl(str(chat_id))
@@ -81,6 +82,32 @@ def update_flood(chat_id: str, user_id) -> bool:
         CHAT_FLOOD[str(chat_id)] = (user_id, count, limit)
         return False
 
+
+def get_flood_limit(chat_id):
+    return CHAT_FLOOD.get(str(chat_id), DEF_OBJ)[2]
+
+
+def set_flood_strength(chat_id, flood_type, value):
+    # for flood_type
+    # 1 = ban
+    # 2 = kick
+    # 3 = mute
+    # 4 = tban
+    # 5 = tmute
+    with INSERTION_FLOOD_SETTINGS_LOCK:
+        curr_setting = SESSION.query(FloodSettings).get(str(chat_id))
+        if not curr_setting:
+            curr_setting = FloodSettings(
+                chat_id, flood_type=int(flood_type), value=value
+            )
+
+        curr_setting.flood_type = int(flood_type)
+        curr_setting.value = str(value)
+
+        SESSION.add(curr_setting)
+        SESSION.commit()
+
+
 def get_flood_setting(chat_id):
     try:
         setting = SESSION.query(FloodSettings).get(str(chat_id))
@@ -91,24 +118,10 @@ def get_flood_setting(chat_id):
 
     finally:
         SESSION.close()
-        
-
-        count += 1
-        if count > limit:  # too many msgs, kick
-            CHAT_FLOOD[str(chat_id)] = (None, DEF_COUNT, limit)
-            return True
-
-        # default -> update
-        CHAT_FLOOD[str(chat_id)] = (user_id, count, limit)
-        return False
-
-
-def get_flood_limit(chat_id):
-    return CHAT_FLOOD.get(str(chat_id), DEF_OBJ)[2]
 
 
 def migrate_chat(old_chat_id, new_chat_id):
-    with INSERTION_LOCK:
+    with INSERTION_FLOOD_LOCK:
         flood = SESSION.query(FloodControl).get(str(old_chat_id))
         if flood:
             CHAT_FLOOD[str(new_chat_id)] = CHAT_FLOOD.get(str(old_chat_id), DEF_OBJ)
