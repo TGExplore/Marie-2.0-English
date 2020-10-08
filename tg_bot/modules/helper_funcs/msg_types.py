0ca140aa39dfdc46d8146754264acb963872f902
Singh <50352435+AVATARX0TIC@users.noreply.github.com>
2020-10-08 17:54:56 +0530

    Update msg_types.py

--- tg_bot/modules/helper_funcs/msg_types.py
+++ tg_bot/modules/helper_funcs/msg_types.py
@@ -17,6 +17,7 @@ class Types(IntEnum):
     VIDEO = 7
     VIDEO_NOTE = 8
 
+
 def get_note_type(msg: Message):
     data_type = None
     content = None
@@ -37,7 +38,7 @@ def get_note_type(msg: Message):
             data_type = Types.TEXT
 
     elif msg.reply_to_message:
-        entities = msg.reply_to_message.parse_entities() or msg.reply_to_message.parse_caption_entities()
+        entities = msg.reply_to_message.parse_entities()
         msgtext = msg.reply_to_message.text or msg.reply_to_message.caption
         if len(args) >= 2 and msg.reply_to_message.text:  # not caption, text
             text, buttons = button_markdown_parser(msgtext,
@@ -90,21 +91,20 @@ def get_welcome_type(msg: Message):
     content = None
     text = ""
 
-    args = msg.text.split(None, 1)  # use python's maxsplit to separate cmd and args
-
-    buttons = []
-    # determine what the contents of the filter are - text, image, sticker, etc
-    if len(args) >= 2:
-        offset = len(args[1]) - len(msg.text)  # set correct offset relative to command + notename
-        text, buttons = button_markdown_parser(args[1], entities=msg.parse_entities(), offset=offset)
-        if buttons:
-            data_type = Types.BUTTON_TEXT
+    try:
+        if msg.reply_to_message:
+            if msg.reply_to_message.text:
+                args = msg.reply_to_message.text
+            else:
+                args = msg.reply_to_message.caption
         else:
-            data_type = Types.TEXT
+            args = msg.text.split(None, 1)  # use python's maxsplit to separate cmd and args
+    except AttributeError:
+        args = False
 
-    elif msg.reply_to_message and msg.reply_to_message.sticker:
+    if msg.reply_to_message and msg.reply_to_message.sticker:
         content = msg.reply_to_message.sticker.file_id
-        text = msg.reply_to_message.caption
+        text = None
         data_type = Types.STICKER
 
     elif msg.reply_to_message and msg.reply_to_message.document:
@@ -132,12 +132,148 @@ def get_welcome_type(msg: Message):
         text = msg.reply_to_message.caption
         data_type = Types.VIDEO
 
-    elif msg.reply_to_message.video_note:
-        msgtext = ""
-        if len(args) > 1:
-            msgtext = args[1]
+    elif msg.reply_to_message and msg.reply_to_message.video_note:
         content = msg.reply_to_message.video_note.file_id
-        text, buttons = button_markdown_parser(msgtext, entities=msg.reply_to_message.parse_caption_entities(), offset=0)
+        text = None
         data_type = Types.VIDEO_NOTE
 
+    buttons = []
+    # determine what the contents of the filter are - text, image, sticker, etc
+    if args:
+        if msg.reply_to_message:
+            argumen = msg.reply_to_message.caption if msg.reply_to_message.caption else ""
+            offset = 0 # offset is no need since target was in reply
+            entities = msg.reply_to_message.parse_entities()
+        else:
+            argumen = args[1]
+            offset = len(argumen) - len(msg.text)  # set correct offset relative to command + notename
+            entities = msg.parse_entities()
+        text, buttons = button_markdown_parser(argumen, entities=entities, offset=offset)
+
+    if not data_type:
+        if text and buttons:
+            data_type = Types.BUTTON_TEXT
+        elif text:
+            data_type = Types.TEXT
+
     return text, data_type, content, buttons
+
+def get_message_type(msg: Message):
+    data_type = None
+    content = None
+    text = ""
+    raw_text = msg.text or msg.caption
+    args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
+
+    buttons = []
+    # determine what the contents of the filter are - text, image, sticker, etc
+    if len(args) >= 2:
+        offset = len(args[1]) - len(raw_text)  # set correct offset relative to command + notename
+        text, buttons = button_markdown_parser(args[1], entities=msg.parse_entities() or msg.parse_caption_entities(),
+                                               offset=offset)
+        if buttons:
+            data_type = Types.BUTTON_TEXT
+        else:
+            data_type = Types.TEXT
+
+    elif msg.reply_to_message:
+        entities = msg.reply_to_message.parse_entities()
+        msgtext = msg.reply_to_message.text or msg.reply_to_message.caption
+        if len(args) >= 1 and msg.reply_to_message.text:  # not caption, text
+            text, buttons = button_markdown_parser(msgtext,
+                                                   entities=entities)
+            if buttons:
+                data_type = Types.BUTTON_TEXT
+            else:
+                data_type = Types.TEXT
+
+        elif msg.reply_to_message.sticker:
+            content = msg.reply_to_message.sticker.file_id
+            data_type = Types.STICKER
+
+        elif msg.reply_to_message.document:
+            content = msg.reply_to_message.document.file_id
+            text, buttons = button_markdown_parser(msgtext, entities=entities)
+            data_type = Types.DOCUMENT
+
+        elif msg.reply_to_message.photo:
+            content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
+            text, buttons = button_markdown_parser(msgtext, entities=entities)
+            data_type = Types.PHOTO
+
+        elif msg.reply_to_message.audio:
+            content = msg.reply_to_message.audio.file_id
+            text, buttons = button_markdown_parser(msgtext, entities=entities)
+            data_type = Types.AUDIO
+
+        elif msg.reply_to_message.voice:
+            content = msg.reply_to_message.voice.file_id
+            text, buttons = button_markdown_parser(msgtext, entities=entities)
+            data_type = Types.VOICE
+
+        elif msg.reply_to_message.video:
+            content = msg.reply_to_message.video.file_id
+            text, buttons = button_markdown_parser(msgtext, entities=entities)
+            data_type = Types.VIDEO
+
+        elif msg.reply_to_message.video_note:
+            content = msg.reply_to_message.video_note.file_id
+            text, buttons = button_markdown_parser(msgtext, entities=entities)
+            data_type = Types.VIDEO_NOTE
+
+    return text, data_type, content, buttons
+
+
+def get_filter_type(msg: Message):
+
+    if msg.text and len(msg.text.split()) >= 3:
+        content = None
+        text = msg.text.split(None, 2)[2]
+        data_type = Types.TEXT
+
+    elif msg.reply_to_message and msg.reply_to_message.text and len(msg.text.split()) >= 2:
+        content = None
+        text = msg.reply_to_message.text
+        data_type = Types.TEXT
+
+    elif msg.reply_to_message and msg.reply_to_message.sticker:
+        content = msg.reply_to_message.sticker.file_id
+        text = None
+        data_type = Types.STICKER
+
+    elif msg.reply_to_message and msg.reply_to_message.document:
+        content = msg.reply_to_message.document.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.DOCUMENT
+
+    elif msg.reply_to_message and msg.reply_to_message.photo:
+        content = msg.reply_to_message.photo[-1].file_id  # last elem = best quality
+        text = msg.reply_to_message.caption
+        data_type = Types.PHOTO
+
+    elif msg.reply_to_message and msg.reply_to_message.audio:
+        content = msg.reply_to_message.audio.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.AUDIO
+
+    elif msg.reply_to_message and msg.reply_to_message.voice:
+        content = msg.reply_to_message.voice.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.VOICE
+
+    elif msg.reply_to_message and msg.reply_to_message.video:
+        content = msg.reply_to_message.video.file_id
+        text = msg.reply_to_message.caption
+        data_type = Types.VIDEO
+
+    elif msg.reply_to_message and msg.reply_to_message.video_note:
+        content = msg.reply_to_message.video_note.file_id
+        text = None
+        data_type = Types.VIDEO_NOTE
+
+    else:
+        text = None
+        data_type = None
+        content = None
+
+    return text, data_type, content
