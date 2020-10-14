d3ff4b6f329598aefd6736cd53545d41dd0122fd
Singh <50352435+AVATARX0TIC@users.noreply.github.com>
2020-10-14 13:27:56 +0530

    Update global_bans.py

--- tg_bot/modules/global_bans.py
+++ tg_bot/modules/global_bans.py
@@ -137,7 +137,7 @@ def gban(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.HTML,
             )
 
-       else:
+    else:
             message.reply_text(
                 "This user is already gbanned, but had no reason set; I've gone and updated it!"
             )
