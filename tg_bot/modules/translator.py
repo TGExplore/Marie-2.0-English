872e52e8fa7744dea021f123f337f8fa53578560
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2021-01-23 10:51:34 +0530

    Update translator.py
    This is a good tr code you must apply.... Thanks

--- tg_bot/modules/translator.py
+++ tg_bot/modules/translator.py
@@ -1,25 +1,59 @@
+from gpytranslate import Translator
+from pyrogram import filters
+from pyrogram.types import Message
 
-from telegram import Message, Update, Bot, User
-from telegram.ext import Filters, MessageHandler, run_async
+from tg_bot import pg
 
-from requests import get
 
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot import dispatcher
+trans = Translator()
 
-base_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-api_key = 'trnsl.1.1.20180603T023816Z.763b39e3388b46d6.aa9abf45baceb438c96bb1593ce58199cc66c4f1'
 
-@run_async
-def translate(bot: Bot, update: Update):
-  message = update.effective_message
-  text = message.reply_to_message.text
-  translation = get(f'{base_url}?key={api_key}&text={text}&lang=en').json()
-  
-  reply_text = f"Language: {translation['lang']}\nText: {translation['text'][0]}"
-  
-  message.reply_to_message.reply_text(reply_text)
+@pg.on_message(filters.command(["tl", "tr"]))
+async def translate(_, message: Message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("Reply to a message to translate it!")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate,
+                              sourcelang=source, targetlang=dest)
+    reply = f"<b>Translated from {source} to {dest}</b>:\n" \
+        f"<code>{translation.text}</code>"
 
-translate_handler = DisableAbleCommandHandler("tl", translate)
+    await message.reply_text(reply, parse_mode="html")
 
-dispatcher.add_handler(translate_handler)
+
+@pg.on_message(filters.command("langs"))
+async def languages(_, message: Message) -> None:
+    await message.reply_text(
+        "Click [here](https://cloud.google.com/translate/docs/languages)"
+        " to see the list of supported language codes!",
+        disable_web_page_preview=True
+    )
+
+
+__mod_name__ = "Translation"
+
+__help__ = """
+Use this module to translate stuff... duh!
+*Commands:*
+• `/tl` (or `/tr`): as a reply to a message, translates it to English.
+• `/tl <lang>`: translates to <lang>
+eg: `/tl ja`: translates to Japanese.
+• `/tl <source>//<dest>`: translates from <source> to <lang>.
+eg: `/tl ja//en`: translates from Japanese to English.
+• `/langs`: get a list of supported languages for translation."""
