c7e7a49fbd36789c9818c10c1ea93f290e66e037
AbSanji <89430810+AbSanji@users.noreply.github.com>
2021-08-24 13:18:50 +0800

    __mains__.py

--- tg_bot/__mains__.py
+++ tg_bot/__mains__.py
@@ -18,29 +18,21 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Ohayou {}. My name is {} nice to meet you kya~
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+Type /help to see my command~
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+im a group manager bot maintained by  [𝒶𝒷 𝓈𝒶𝓃𝒿𝒾](tg://user?id={}).
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
-
-For more commands click /help...
-
-**Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
+**Enjoy~**
 
 """
 
 HELP_STRINGS = """
 
-Hello! my name *{}*.
-
 *Main* available commands:
  - /start: Start the bot...
  - /help: help....
- - /donate: To find out more about donating!
  - /settings:
    - in PM: To find out what SETTINGS you have set....
    - in a group:
@@ -48,12 +40,6 @@ Hello! my name *{}*.
 {}
 And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll of the following commands  / or ! can  be used...\n")
-
-DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
